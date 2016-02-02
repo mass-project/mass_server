0e34756650a1f5b205838e8ad4fe640b42b12432
Fabian Rump <rumpf@cs.uni-bonn.de>
2016-02-02 12:13:17 +0100

    Bumping changes to main repo. Adding requirements and venv builder.

---
+++ make_venv.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -e
+
+# get script location
+
+pushd `dirname $0` > /dev/null
+SCRIPTPATH=`pwd`
+popd > /dev/null
+
+cd ${SCRIPTPATH}
+
+# set python version
+PYTHON_BIN=$(which python3)
+
+# set venv name
+VENV=venv_mass
+
+# delete previous virtual environment if existing
+rm -rf ${VENV}
+
+# create new virtual environment
+virtualenv --python ${PYTHON_BIN} --no-site-packages ${VENV}
+
+# activate virtual environment
+source ${VENV}/bin/activate
+
+# install required packages
+pip install -r requirements_upgrade_pip.txt
+pip install -r requirements_upgrade_setuptools.txt
+pip install -r requirements_upgrade_wheel.txt
+pip install -r requirements.txt
--- mass_server_flask.py
+++ mass_server_flask.py
@@ -1,6 +1,5 @@
-from apispec import APISpec
-from flask import request, jsonify
-from flask.views import MethodView
+import os
+from flask import send_from_directory
 
 from mass_flask_config import app
 from mass_flask_api.config import api_blueprint
@@ -10,6 +9,14 @@ from mass_flask_core.signals import connect_signals
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+ui_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "mass_angular_webui")
+
+
+@app.route('/webui/<path:path>', methods=['GET'])
+def static_proxy(path):
+    return send_from_directory(ui_path, path)
 
 
 if __name__ == '__main__':
