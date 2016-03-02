2ae2310cf6a343cd675b93ec1713542ab2ed7490
Fabian Rump <rumpf@cs.uni-bonn.de>
2016-03-02 13:50:06 +0100

    Adding secret key generation, related to MASS-116.

--- mass_flask_config/app.py
+++ mass_flask_config/app.py
@@ -3,8 +3,31 @@ import subprocess
 from flask import Flask
 from flask.ext.mongoengine import MongoEngine
 
+# Initialize app
 app = Flask(__name__)
+
+# Generate or load secret key
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+SECRET_FILE = os.path.join(BASE_DIR, 'secret.txt')
+try:
+    app.secret_key = open(SECRET_FILE).read().strip()
+except IOError:
+    try:
+        import random
+        app.secret_key = ''.join([random.SystemRandom().choice('abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)') for i in range(50)])
+        secret = open(SECRET_FILE, 'w')
+        secret.write(app.secret_key)
+        secret.close()
+    except IOError:
+        Exception('Please create a %s file with random characters \
+        to generate your secret key!' % SECRET_FILE)
+
+# Load config
 config_path = os.getenv('CONFIG_PATH', 'mass_flask_config.config_development.DevelopmentConfig')
 app.config.from_object(config_path)
+
+# Init db
 db = MongoEngine(app)
+
+# Load app version from git
 app.version = subprocess.check_output(['git', 'describe'], cwd=os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
