3467043dff31843608f1eed7c0d3e58877489c23
Fabian Rump <rumpf@cs.uni-bonn.de>
2016-04-05 13:37:41 +0200

    Moving Pagination from API to core since it is a shared function for both API and webui

---
+++ mass_flask_core/utils/pagination_functions.py
@@ -0,0 +1,33 @@
+from functools import wraps
+from math import ceil
+
+from flask import url_for, request
+
+
+def _get_page_link(page_number):
+    return url_for(request.url_rule.endpoint, page=page_number, _external=True)
+
+
+class PaginationFunctions:
+    @staticmethod
+    def paginate(per_page=100):
+        def real_decorator(view_function):
+            @wraps(view_function)
+            def paginate_function(*args, **kwargs):
+                if 'page' in request.args:
+                    page = int(request.args['page'])
+                else:
+                    page = 1
+                queryset = view_function(*args, **kwargs)
+                page_count = ceil(len(queryset)/per_page)
+                paginated_queryset = queryset.paginate(page=page, per_page=per_page)
+                result = {
+                    'results': paginated_queryset.items,
+                    'next': _get_page_link(paginated_queryset.next_num) if paginated_queryset.has_next else None,
+                    'previous': _get_page_link(paginated_queryset.prev_num) if paginated_queryset.has_prev else None,
+                    'page': page,
+                    'page_count': page_count
+                }
+                return result
+            return paginate_function
+        return real_decorator
