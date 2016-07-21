bd680b95c4901a1fab5c91d8b320f33637d573d2
Fabian Rump <rumpf@cs.uni-bonn.de>
2016-07-21 13:58:10 +0200

    Some added functionality to better integrate the new views. Eye-candy ftw.

--- mass_flask_webui/views/login.py
+++ mass_flask_webui/views/login.py
@@ -1,5 +1,5 @@
 from flask import flash, render_template, redirect, url_for
-from flask.ext.login import login_user, login_required, logout_user
+from flask.ext.login import login_user, login_required, logout_user, current_user
 
 from mass_flask_webui.config import webui_blueprint
 from mass_flask_webui.forms.login import LoginForm
@@ -7,11 +7,15 @@ from mass_flask_webui.forms.login import LoginForm
 
 @webui_blueprint.route('/login/', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        login_user(form.user)
-        flash('Logged in successfully!', 'success')
-    return render_template('login.html', form=form)
+    if current_user.is_authenticated:
+        return redirect(url_for('.profile'))
+    else:
+        form = LoginForm()
+        if form.validate_on_submit():
+            login_user(form.user)
+            flash('Logged in successfully!', 'success')
+            return redirect(url_for('.profile'))
+        return render_template('login.html', form=form)
 
 
 @webui_blueprint.route('/logout/', methods=['GET'])
