0876542a9c4d28bcbd50f79bf674fd58019c5248
Fabian Rump <rumpf@cs.uni-bonn.de>
2016-08-25 20:54:30 +0200

    Huge refactoring of the authentication system. Dropping some code in favor of flask_modular_auth

--- mass_flask_config/app.py
+++ mass_flask_config/app.py
@@ -1,9 +1,10 @@
 import os
 import subprocess
 
-from flask import Flask
-from flask.ext.bootstrap import Bootstrap
-from flask.ext.mongoengine import MongoEngine
+from flask import Flask, redirect, url_for, request, flash
+from flask_bootstrap import Bootstrap
+from flask_mongoengine import MongoEngine
+from flask_modular_auth import AuthManager, current_authenticated_entity, SessionBasedAuthProvider, KeyBasedAuthProvider
 from .reverse_proxy import ReverseProxied
 
 # Initialize app
@@ -35,6 +36,27 @@ db = MongoEngine(app)
 
 # Init flask-bootstrap
 Bootstrap(app)
+
+
+# Init auth system
+def setup_session_auth(user_loader):
+    app.session_provider = SessionBasedAuthProvider(user_loader)
+    auth_manager.register_auth_provider(app.session_provider)
+
+
+def setup_key_based_auth(key_loader):
+    app.key_based_provider = KeyBasedAuthProvider(key_loader)
+    auth_manager.register_auth_provider(app.key_based_provider)
+
+
+def unauthorized_callback():
+    if current_authenticated_entity.is_authenticated:
+        flash('You are not authorized to access this resource!', 'warning')
+        return redirect(url_for('mass_flask_webui.index'))
+    else:
+        return redirect(url_for('mass_flask_webui.login', next=request.url))
+
+auth_manager = AuthManager(app, unauthorized_callback=unauthorized_callback)
 
 # Load app version from git
 app.version = subprocess.check_output(['git', 'describe'], cwd=os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')).decode().strip()
