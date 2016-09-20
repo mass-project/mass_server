e5b44669fc7a4a8a63ba23c53d3072d25be5c310
Fabian Rump <rumpf@cs.uni-bonn.de>
2016-09-20 14:33:15 +0200

    Cleaning up the test config.

--- test.sh
+++ test.sh
@@ -1,25 +1,2 @@
 #!/usr/bin/env bash
-set -e
-
-# get script location
-
-pushd `dirname $0` > /dev/null
-SCRIPTPATH=`pwd`
-popd > /dev/null
-
-cd ${SCRIPTPATH}
-
-export CONFIG_PATH=mass_flask_config.config_testing.TestingConfig
-
-nosetests \
-    --verbosity 2 \
-    --with-coverage \
-    --cover-package=mass_flask_api \
-    --cover-package=mass_flask_core \
-    --cover-package=mass_flask_config \
-    --cover-inclusive \
-    --cover-erase \
-    --cover-branches \
-    --cover-xml \
-    --with-xunit \
-    --exclude-dir=./venv_mass/
+CONFIG_PATH=mass_flask_config.config_testing.TestingConfig nosetests
