f7d291c4b00bddd7457b82df8bc287d8e0ce17d6
Timm Behner <behner@fkie.fraunhofer.de>
2017-03-01 10:19:54 +0100

    Return 201 and Report object

--- mass_flask_api/resources/scheduled_analysis.py
+++ mass_flask_api/resources/scheduled_analysis.py
@@ -9,6 +9,7 @@ from mass_flask_api.utils import get_pagination_compatible_schema, register_api_
 from mass_flask_api.schemas import ScheduledAnalysisSchema, ReportSchema
 from mass_flask_core.models import ScheduledAnalysis
 
+import logging
 
 class ScheduledAnalysisResource(BaseResource):
     schema = ScheduledAnalysisSchema()
@@ -138,7 +139,8 @@ class ScheduledAnalysisResource(BaseResource):
 
                 report.save()
                 scheduled_analysis.delete()
-                return '', 204
+                return jsonify(ReportSchema().dump(report).data), 201
+                # return '', 204
         except DoesNotExist:
             return jsonify({'error': 'No object with key \'{}\' found'.format(kwargs['id'])}), 404
 
