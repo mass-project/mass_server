bf0992d82a307e63b2abb9b1f309cb6a4364a538
Fabian Rump <rumpf@cs.uni-bonn.de>
2017-04-20 11:37:34 +0200

    Fix temp production config

--- mass_server/config/config_production.py
+++ mass_server/config/config_production.py
@@ -5,7 +5,7 @@ class ProductionConfig(BaseConfig):
     DEBUG = False
     MONGODB_SETTINGS = {
         'db': 'mass-flask-production',
-        'host': 'localhost',
+        'host': 'mongodb',
         'port': 27017,
         'tz_aware': True
     }
