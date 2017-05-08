fb35f71d9a2dcaea3e5d228fb343e604da39b428
Christopher Schmidt <christopher.schmidt@posteo.de>
2017-05-08 12:21:15 +0200

    Made changes requested in #64

--- mass_flask_webui/views/sample.py
+++ mass_flask_webui/views/sample.py
@@ -24,34 +24,16 @@ def sample_list():
 def sample_detail(sample_id):
     try:
         sample = Sample.objects.get_with_tlp_level_filter().get(id=sample_id)
+
         comment_form = CommentForm()
-        if comment_form.validate_on_submit():
-            sample.add_comment(comment_form.data['comment'], TimeFunctions.get_timestamp(), current_authenticated_entity._get_current_object())
-            sample.save()
-            flash('Your comment has been added', 'success')
-            return redirect(url_for('.sample_detail', sample_id=sample.id))
-        reports = Report.objects(sample=sample)
-
-        analysis_systems = []
-        for system in AnalysisSystem.objects:
-            analysis_systems.append((system.identifier_name, system.verbose_name))
-
+        _process_comment_form(comment_form, sample)
         request_form = RequestAnalysisForm()
-        request_form.analysis_system.choices = analysis_systems
-
-        if request_form.validate_on_submit():
-            priority = request_form.data['priority']
-            analysis_system = AnalysisSystem.objects.get(identifier_name=request_form.data['analysis_system'])
-            try:
-                request = AnalysisRequest.objects.get(sample=sample, analysis_system=analysis_system)
-                request.priority = priority
-            except DoesNotExist:
-                request = AnalysisRequest(sample=sample, analysis_system=analysis_system, priority=priority)
-            request.save()
+        _process_request_form(request_form, sample)
 
         requested_analyses = AnalysisRequest.objects(sample=sample)
         scheduled_analyses = ScheduledAnalysis.objects(sample=sample)
 
+        reports = Report.objects(sample=sample)
         activity = [{
             'class': 'info',
             'glyph': 'fa-paper-plane',
@@ -84,11 +66,32 @@ def sample_detail(sample_id):
 
         return render_template('sample_detail.html', sample=sample, reports=reports, activity=sorted_activity,
                                comment_form=comment_form, requested_analyses=requested_analyses,
-                               scheduled_analyses=scheduled_analyses, analysis_systems=analysis_systems,
-                               request_form=request_form)
+                               scheduled_analyses=scheduled_analyses, request_form=request_form)
     except DoesNotExist:
         flash('Sample not found or you do not have access to this sample.', 'warning')
         return redirect(url_for('.index'))
+
+
+def _process_comment_form(form, sample):
+    if form.validate_on_submit():
+        sample.add_comment(form.data['comment'], TimeFunctions.get_timestamp(),
+                           current_authenticated_entity._get_current_object())
+        sample.save()
+        flash('Your comment has been added', 'success')
+        return redirect(url_for('.sample_detail', sample_id=sample.id))
+
+
+def _process_request_form(form, sample):
+    if form.validate_on_submit() and current_authenticated_entity.is_authenticated:
+        priority = form.data['priority']
+        analysis_system = AnalysisSystem.objects.get(identifier_name=form.data['analysis_system'])
+        try:
+            request = AnalysisRequest.objects.get(sample=sample, analysis_system=analysis_system)
+            request.priority = priority
+        except DoesNotExist:
+            request = AnalysisRequest(sample=sample, analysis_system=analysis_system, priority=priority)
+        request.save()
+        flash('Your request has been saved', 'success')
 
 
 @webui_blueprint.route('/sample/<sample_id>/graph/')
