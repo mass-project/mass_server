8a613314be0868e508ff1117a4406471291a775f
Christopher Schmidt <christopher.schmidt@posteo.de>
2017-05-09 16:03:39 +0200

    Automatic sample matching for new analysis system

--- mass_flask_core/signals/dispatch_request.py
+++ mass_flask_core/signals/dispatch_request.py
@@ -6,16 +6,25 @@ def _filter_matches_tags(tags, tag_filter):
     return TagParser(tags).parse_string(tag_filter)
 
 
-def _find_matching_systems_for_sample(sample):
-    for item in AnalysisSystem.objects():
-        if item in sample.dispatched_to:
-            continue
-        if not _filter_matches_tags(sample.tags, item.tag_filter_expression):
-            continue
-        analysis_request = AnalysisRequest(sample=sample, analysis_system=item)
-        analysis_request.save()
-        sample.dispatched_to.append(item)
+def _match_sample_and_system(sample, system):
+    if system in sample.dispatched_to:
+        return
+    if not _filter_matches_tags(sample.tags, system.tag_filter_expression):
+        return
+    analysis_request = AnalysisRequest(sample=sample, analysis_system=system)
+    analysis_request.save()
+    sample.dispatched_to.append(system)
     sample.save()
+
+
+def _find_matching_systems_for_sample(sample):
+    for system in AnalysisSystem.objects():
+        _match_sample_and_system(sample, system)
+
+
+def _find_matching_samples_for_system(system):
+    for sample in Sample.objects():
+        _match_sample_and_system(sample, system)
 
 
 def update_dispatch_request_for_new_sample(sender, document, **kwargs):
@@ -23,3 +32,10 @@ def update_dispatch_request_for_new_sample(sender, document, **kwargs):
         return
     if kwargs.get('created') is True:
         _find_matching_systems_for_sample(document)
+
+
+def create_requests_for_new_analysis_system(sender, document, **kwargs):
+    if not issubclass(sender, AnalysisSystem):
+        return
+    if kwargs.get('created') is True:
+        _find_matching_samples_for_system(document)
