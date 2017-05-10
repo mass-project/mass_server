9bd58ca832504a8638d6f066a4417986aaaecec2
Christopher Schmidt <christopher.schmidt@posteo.de>
2017-05-10 12:27:50 +0200

    Additional testcase where the samples are created first

--- mass_flask_core/signals/__init__.py
+++ mass_flask_core/signals/__init__.py
@@ -6,5 +6,6 @@ from .copy_report_tags import copy_tags_from_report_to_sample
 
 def connect_signals():
     signals.post_save.connect(update_dispatch_request_for_new_sample)
-    signals.post_save.connect(copy_tags_from_report_to_sample, sender=Report)
     signals.post_save.connect(create_requests_for_new_analysis_system, sender=AnalysisSystem)
+    signals.post_save.connect(copy_tags_from_report_to_sample, sender=Report)
+
--- mass_flask_core/tests/test_request_dispatch.py
+++ mass_flask_core/tests/test_request_dispatch.py
@@ -11,6 +11,20 @@ class RequestDispatchTestCase(FlaskTestCase):
         ipsample = mixer.blend(IPSample, tags=['sample-type:ipsample'])
         urisample = mixer.blend(URISample, tags=['sample-type:urisample'])
 
+        self._are_requests_correct(ipsample, urisample, system)
+
+    def test_are_requests_correct_after_analysis_system_creation(self):
+        ipsample = mixer.blend(IPSample, tags=['sample-type:ipsample'])
+        urisample = mixer.blend(URISample, tags=['sample-type:urisample'])
+        system = mixer.blend(AnalysisSystem, tag_filter_expression='sample-type:ipsample')
+
+        # Refresh samples from DB, to reload the dispatched_to field
+        ipsample = IPSample.objects.get(id=ipsample.id)
+        urisample = URISample.objects.get(id=urisample.id)
+
+        self._are_requests_correct(ipsample, urisample, system)
+
+    def _are_requests_correct(self, ipsample, urisample, system):
         self.assertEqual(ipsample.dispatched_to, [system])
         self.assertEqual(urisample.dispatched_to, [])
 
