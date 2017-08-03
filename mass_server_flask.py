b04369367b48509fb53d39e1cf98df083f8af384
Timm Behner <behner@cs.uni-bonn.de>
2017-08-03 09:24:06 +0200

    Added migration to mass_server_flask.py

--- mass_server_flask.py
+++ mass_server_flask.py
@@ -1,7 +1,9 @@
 from mass_flask_config.bootstrap import bootstrap_mass_flask
 from mass_flask_config.app import app
+from mass_flask_config.migration import schema_migration
 
 bootstrap_mass_flask()
+schema_migration()
 
 if __name__ == '__main__':
     app.run()
