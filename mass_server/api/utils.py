8df364f0d569deff0d9494287b56f8b49fc81419
Christopher Schmidt <christopher.schmidt@posteo.de>
2017-11-14 17:35:01 +0100

    Added mapped filter parameters for samples and analysis systems

--- mass_server/api/utils.py
+++ mass_server/api/utils.py
@@ -2,6 +2,7 @@ from flask import url_for, request
 from math import ceil
 from flask_slimrest.pagination import PaginationResult
 from flask_slimrest.utils import make_api_response
+from functools import wraps
 
 
 def _get_page_link(page_number):
@@ -33,3 +34,20 @@ def pagination_helper(queryset, per_page=100):
 
 def filter_queryset(queryset, **kwargs):
     return queryset.filter(**kwargs)
+
+
+def apply_mapped_filters(filter_args={}):
+    def real_decorator(function):
+        @wraps(function)
+        def wrapper(self, *args, **kwargs):
+            passed_args = {}
+            for arg in request.args:
+                if arg in filter_args:
+                    if filter_args[arg]:
+                        arg_name = filter_args[arg]
+                    else:
+                        arg_name = arg
+                    passed_args[arg_name] = request.args[arg]
+            return filter_queryset(function(self, *args, **kwargs), **passed_args)
+        return wrapper
+    return real_decorator
